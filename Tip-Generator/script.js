a6956090d2f787ef255e73274836a2ed9d20c5b1
nullsc <53406164+nullsc@users.noreply.github.com>
2020-07-10 21:37:54 +0100

    Update script.js

--- Tip-Generator/script.js
+++ Tip-Generator/script.js
@@ -2,18 +2,25 @@
 
 var result = document.querySelector('#result');
 
-var cbtn = document.querySelector('.cBtn');
+var cbtn = document.querySelector('#calculateBtn');
+var resetB = document.querySelector('#resetBtn');
+
 var drop = document.getElementById('tAmounts');
-cbtn.addEventListener('click', calculate);
+cbtn.addEventListener('click', calculate); //calculate
 drop.addEventListener('change', updateSer);
+resetB.addEventListener('click', resetForm); //reset
 
 function calculate(){
 	var total = document.getElementById('pBox').value;
 	var tip = document.getElementById('tBox').value;
-	if(tip == "" || total == "")
-		console.log("Error: please enter vals"); //debug
+	if(tip == "" || total == ""){
+		//console.log("Error: please enter vals");
+		result.innerHTML = "Error: please enter values";
+		return false;
+	}
+
 	
-	result.innerHTML = total * (tip/100);
+	result.innerHTML = "£" + total * (tip/100);
 	var drop = document.getElementById('tAmounts').value;
 
 }
@@ -28,5 +35,6 @@ function validate(){
 }
 
 function resetForm(){
-	return 0; //later
+	document.getElementById('pBox').value = "";
+	document.getElementById('tBox').value = "";
 }
