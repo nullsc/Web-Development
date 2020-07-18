469fbf1bb50b8bd860de08f79e07ef3eeb426eb3
nullsc <53406164+nullsc@users.noreply.github.com>
2020-07-19 00:34:22 +0100

    Add files via upload

---
+++ Todo List App/script.js
@@ -0,0 +1,32 @@
+var ul = document.querySelector("ul");
+var btn = document.querySelector("#submit-button");
+btn.addEventListener("click", add);
+
+var input = document.getElementById("userInput");
+
+
+function add() {
+	if (input.value == "")
+		return false;
+	
+	var li = document.createElement("li"); // creates an element "li"
+	li.appendChild(document.createTextNode(input.value)); //makes text from input field the li text
+	ul.appendChild(li); //adds li to ul
+	console.log(input.value);
+	input.value = ""; //Reset text input field
+	
+	function crossOut() { //func inside so only applies to new
+		li.classList.toggle("done"); //toggle the completed class
+	}
+
+	li.addEventListener("click", crossOut);
+	
+	var dBtn = document.createElement("button");
+	dBtn.appendChild(document.createTextNode("X"));
+	li.appendChild(dBtn);
+	dBtn.addEventListener("click", deleteListItem);
+	
+	function deleteListItem(){
+		li.classList.add("delete");
+	}
+}
